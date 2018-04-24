adac400caa348d0846e16953dc4226e8774127c8
David Dean <dd4yd@mail.missouri.edu>
2018-04-24 15:41:42 -0500

    Re created repo bc of small local issue

---
+++ ColorsNavigation/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  ColorDetailViewController.swift
+//  Colors
+//
+//  Created by David Dean on 4/11/18.
+//  Copyright © 2018 David Dean. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
---
+++ ColorsNavigation/Colors/ColorsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ColorDetailViewController.swift
+//  Colors
+//
+//  Created by David Dean on 4/11/18.
+//  Copyright © 2018 David Dean. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple),
+                  Color(name: "brown", uiColor: UIColor.brown)]
+
+    @IBOutlet weak var colorsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Colors"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        let color = colors[indexPath.row]
+        
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.isSelected = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //'as': if cant cast as ColorDetailViewController -> nil
+        //setting color on dest view controller, dest and row must not be nil or it wont be called (unwrapping)
+        if let destination = segue.destination as? ColorDetailViewController,
+           let row = colorsTableView.indexPathForSelectedRow?.row {
+            destination.color = colors[row]
+        }
+        
+    }
+
+}
